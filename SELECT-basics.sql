44894eab0a0112a89c375e6d14ede8ddb155f8fb
Ayush Kumar Pandey <80335585+ayushp01@users.noreply.github.com>
2021-05-31 18:49:09 +0530

    Update

--- SELECT-basics.sql
+++ SELECT-basics.sql
@@ -1,9 +1,9 @@
-*/ 
+/*
 First section of sqlzoo
 */
 
-*/
-#1 The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
+#1
+/* The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
 
 Modify it to show the population of Germany
 */
@@ -11,3 +11,18 @@ Modify it to show the population of Germany
 SELECT population FROM world
 WHERE name = 'Germany'
   
+#2
+/*Checking a list The word IN allows us to check if an item is in a list.
+This query shows the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+*/
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+  
+#3
+/* Just the right size
+
+Which countries are not too small and not too big? BETWEEN allows range checking (range specified is inclusive of boundary values).
+This query shows the country and the area for countries with an area between 200,000 and 250,000.
+*/
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
